01fa75ef12f7dc3e716ebefd6d4acc97f1c74c3d
szakyfx <szakacsyp@gmail.com>
2025-11-19 18:50:31 +0100

    Update Neurális háló.py

--- Neurális háló.py
+++ Neurális háló.py
@@ -7,12 +7,12 @@ from sklearn.metrics import mean_squared_error
 #Adatletöltés
 df = yf.download("EURUSD=X", period="1y", interval="1d")[["Close"]].dropna()
 
-#Hozam és előző napi hozam kiszámítása
+#Hozam
 df["Return"] = df["Close"].pct_change()
 df["Prev_Return"] = df["Return"].shift(1)
 df = df.dropna()
 
-#Train-test split
+#Szétválasztás
 split = int(len(df) * 0.8)
 X = df[["Prev_Return"]].values
 y = df["Return"].values
@@ -28,8 +28,9 @@ y_train, y_test = y[:split], y[split:]
 mlp = MLPRegressor(hidden_layer_sizes=(8, 4), max_iter=500, random_state=42)
 mlp.fit(X_train, y_train)
 
-#Előrejelzés és értékelés
+#Előrejelzés
 y_pred = mlp.predict(X_test)
 mse_nn = mean_squared_error(y_test, y_pred)
 
 print(f"MLP modell MSE: {mse_nn:.6f}")
+
